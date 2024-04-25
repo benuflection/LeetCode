c5e06ba61b11b291201276a71943aa3c74ef0d62
benuflection <39638302+benuflection@users.noreply.github.com>
2024-04-24 21:51:00 -0400

    topk

---
+++ topk.py
@@ -0,0 +1,25 @@
+
+class Solution:
+    def __init__(self, nums, k):
+        self.nums = nums
+        self.k = k
+
+    def topKFrequent(self, nums, k):
+
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+        res = []
+        for i in range(len(freq)-1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+nums = [1,1,1,2,2,3]
+k = 2
+solution = Solution(nums, k)
+print(solution.topKFrequent(nums, k))
